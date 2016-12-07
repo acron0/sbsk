2a6b1a7bbcf059f970f09a7ddd3fe6a4bd698f94
Antony Woods <antony@mastodonc.com>
2016-12-07 21:49:54 +0000

    Fix typo in script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 SBSK_DIR=/home/sbsk/sbsk-app
 if [ -d "$SBSK_DIR" ]; then
-    rm -rf %SBSK_DIR
+    rm -rf $SBSK_DIR
 fi
 VERSION=$(curl "https://api.github.com/repos/acron0/sbsk/tags" 2> /dev/null | jq '.[0].name' | sed 's/^.\(.*\).$/\1/')
 wget -O sbsk.zip "https://github.com/acron0/sbsk/archive/$VERSION.zip"
