80ae182b4131c5bb932454e7d1bab053326d2b09
Antony Woods <antony@mastodonc.com>
2017-04-12 21:42:12 +0100

    Fix issue with daemon

--- scripts/build-daemon.sh
+++ scripts/build-daemon.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 export SBSK_ADMIN_ADDRESS=sbskadmin.teamwoods.org
 export SBSK_ADMIN_PORT=7000
+export SBSK_SEARCH_ADDRESS=sbskadmin.teamwoods.org
+export SBSK_SEARCH_PORT=3000
 lein do \
      clean, \
      cljsbuild once min-admin, \
