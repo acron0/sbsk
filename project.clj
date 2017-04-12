3313271d94593cc75486ea428e53c415fac3fb69
Antony Woods <antony@mastodonc.com>
2017-04-12 21:44:43 +0100

    Version 0.2.15-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject sbsk "0.2.14"
+(defproject sbsk "0.2.15-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
